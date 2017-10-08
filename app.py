9717bb4da86d0cc0f108db31d5ff8e01f2766295
AAA <aaa@aaa.com>
2017-10-08 16:24:15 -0500

    User accounts implemented, need store page/widget database

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, session, redirect, request
 import models as dbHandler
-
+import os
 app = Flask(__name__)
 
 
@@ -26,8 +26,30 @@ def index():
 	else:
 		return render_template('index.html', message=message)
 	
-
-
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	message=None
+	if request.method=='POST':
+		username = request.form['username']
+		password = request.form['password']
+		if dbHandler.userExists(username) == True:
+			message="UserAlready exists"
+			return render_template('signup.html', message=message)
+		else:
+			#insert new user
+			dbHandler.insertUser(username, password)
+			return render_template('store.html', username=username)
+                               
+	else:
+		return render_template('signup.html', message=message)
+    
+@app.route('/store', methods=['GET', 'POST'])
+def store():
+	if not session.get('logged_in'):
+		return "You do not have access to this page"
+	else:
+		return render_template('store.html', username=username)
+    
 
 
 if __name__ == '__main__':
