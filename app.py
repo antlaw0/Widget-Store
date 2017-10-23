6ce6e941788f953d9875c1cbdc0a307e73052885
AAA <aaa@aaa.com>
2017-10-22 19:15:28 -0500

    implementing postgresql alchemy

--- app.py
+++ app.py
@@ -1,57 +1,46 @@
-from flask import Flask, render_template, flash, session, redirect, request
-import models as dbHandler
 import os
+
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-	message=None
-	if request.method=='POST':
-		username = request.form['username']
-		password = request.form['password']
-		#if entered username exists
-		if dbHandler.userExists(username) == True:
-			#if password matches password in database
-			if dbHandler.getPassword(username) == password:
-				return render_template('store.html', username=username)
-			else:
-				message="Invalid password"
-				return render_template('index.html', message=message)		
-        
-		else:	   
-			message="User does not exist"
-			return render_template('index.html', message=message)		
-        
-	else:
-		return render_template('index.html', message=message)
-	
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-	message=None
-	if request.method=='POST':
-		username = request.form['username']
-		password = request.form['password']
-		if dbHandler.userExists(username) == True:
-			message="UserAlready exists"
-			return render_template('signup.html', message=message)
-		else:
-			#insert new user
-			dbHandler.insertUser(username, password)
-			return render_template('store.html', username=username)
-                               
-	else:
-		return render_template('signup.html', message=message)
-    
-@app.route('/store', methods=['GET', 'POST'])
-def store():
-	if not session.get('logged_in'):
-		return "You do not have access to this page"
-	else:
-		return render_template('store.html', username=username)
-    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
 
 
-if __name__ == '__main__':
-	app.secret_key = os.urandom(12)
-	app.run(debug=True)
+@app.route('/')
+def home():
+	#user = User('Jane Appleton', 'Jane.appleton@example.com')
+	#db.session.add(user)
+	#db.session.commit()
+	all_users = User.query.all()
+	#name= all_users[0].name
+	users=[]
+	for i in all_users:
+		users.append(all_users[i].name)
+	return render_template('index.html', users=users)
+
+
+@app.route('/robots.txt')
+def robots():
+    res = app.make_response('User-agent: *\nAllow: /')
+    res.mimetype = 'text/plain'
+    return res
+
+#if __name__ == '__main__':
+#port = int(os.environ.get('PORT', 5000))
+#app.run(host='127.0.0.1', port=port, debug=True)
